011117e3152eebfc4c67eb06323f88a222847558
origin <surgeon712@gmail.com>
2024-10-18 21:54:22 +0300

    сделано 3 задачи

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        byte apple = 100;
+        System.out.println("Значение переменной с типом byte =" + apple);
+        short orange = 15000;
+        System.out.println("Значение переменной с типом short = " + orange);
+        int bananas = 50000;
+        System.out.println("Значение переменной с типом int = " + bananas);
+        long year = 360000000L;
+        System.out.println("Значение переменной с типом long = " + year);
+        float c = 3.78F;
+        System.out.println("Значение переменной с типом float = " + c);
+        double b = 1.7d;
+        System.out.print("Значение переменной с типом doble = " + b);
+        float cent = 27.12F;
+        System.out.println(cent);
+        long e = 987678965549L;
+        System.out.println(e);
+        double j = 2.786d;
+        System.out.print(j);
+        short k = 569;
+        System.out.print(k);
+        short l = -159;
+        System.out.print(l);
+        short p = 27897;
+        System.out.print(p);
+        byte w = 67;
+        System.out.print(w);
+        byte LudmilaPavlovna = 23;
+        byte AnnaSergeevna = 27;
+        byte EkaterinaAndrrevna = 30;
+        short allSheets = 480;
+        System.out.println("На каждого ученика рассчитано " + allSheets / (LudmilaPavlovna + AnnaSergeevna + EkaterinaAndrrevna) + " листов бумаги");
+
+
     }
 }
